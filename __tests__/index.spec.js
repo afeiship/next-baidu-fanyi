173211a81c78f47589350ccb67482ef4875507a1
afeiship <1290657123@qq.com>
2019-12-26 12:14:23 +0800

    add: translate to cht

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,7 +3,6 @@
   var NxBaiduFanyi = require('../src/next-baidu-fanyi');
   var sleep = require('sleep');
 
-
   describe('NxBaiduFanyi.methods', function() {
     test('translate apple to english', function(done) {
       var option = {
@@ -36,6 +35,25 @@
           from: 'zh',
           to: 'de',
           trans_result: [{ src: '苹果', dst: 'Apfel' }]
+        });
+        done();
+      });
+    });
+
+    test('translate apple to cht', function(done) {
+      var option = {
+        q: '你好计算机',
+        to: 'cht'
+      };
+
+      console.log('sleep 1s');
+      sleep.sleep(1);
+
+      NxBaiduFanyi.translate(option).then((res) => {
+        expect(res).toEqual({
+          from: 'zh',
+          to: 'cht',
+          trans_result: [{ src: '你好计算机', dst: '你好電腦' }]
         });
         done();
       });
